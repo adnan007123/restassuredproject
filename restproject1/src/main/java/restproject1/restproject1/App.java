b7a9e8afb20a8a2d77a0e8867e79d1ab887b5890
admin <admin@197nodnb11637t1.one97.delhi.net>
2020-05-02 12:51:27 +0530

    this is my first commit

---
+++ restproject1/src/main/java/restproject1/restproject1/App.java
@@ -0,0 +1,13 @@
+package restproject1.restproject1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
